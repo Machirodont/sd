aec7cd3f73ae23c187f2d2ed58d4eeafe12d6d7b
Ivanitsky Alexey <a.ivanitsky@lan-project.ru>
2019-03-05 14:19:59 +0300

    Sql Dump Controller

---
+++ commands/DumpController.php
@@ -0,0 +1,40 @@
+<?php
+/**
+ * @link http://www.yiiframework.com/
+ * @copyright Copyright (c) 2008 Yii Software LLC
+ * @license http://www.yiiframework.com/license/
+ */
+
+namespace app\commands;
+
+use yii\console\Controller;
+use yii\console\ExitCode;
+
+/**
+ * This command echoes the first argument that you have entered.
+ *
+ * This command is provided as an example for you to learn how to create console commands.
+ *
+ * @author Qiang Xue <qiang.xue@gmail.com>
+ * @since 2.0
+ */
+class HelloController extends Controller
+{
+
+    public function actionSave()
+    {
+        if (preg_match('/dbname=([^;]*)/', \Yii::$app->db->dsn, $match)) {
+            $dbname=$match[1];
+            $dbuser=\Yii::$app->db->username;
+            $dbpass=\Yii::$app->db->password;
+            echo $dbname.':'.$dbuser.':'.$dbpass;
+            echo "\n";
+            shell_exec("del sql.sql");
+            echo "\n";
+            shell_exec("mysqldump -c -Q -u ".$dbuser." -p".$dbpass." ".$dbname." > sql.sql");
+        } else {
+            echo "Error: can't get DB name: p." . __LINE__ . " f." . __FILE__;
+        }
+        return ExitCode::OK;
+    }
+}
